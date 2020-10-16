0d046b31242ea7562dfd9d727fd15b71a4ed538f
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-16 10:37:44 -0400

    created player class

--- war.py
+++ war.py
@@ -1,17 +1,5 @@
 from random import shuffle
 
-class Deck:
-    def __init__(self):
-        self.cards = []
-        for i in range(2, 15):
-            for j in range(4):
-                self.cards.append(card(i, j))
-        shuffle(self.cards)
-
-    def rm_card(self):
-        if len(self.cards) == 0:
-            return
-        return self.cards.pop()
 class Card:
     suits = ("spades",
             "hearts",
@@ -49,5 +37,21 @@ class Card:
         v = self.values[self.value] + " of " + self.suits[self.suit]
         return v
 
-card = Card(3, 2)
-print(card)
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards.append(Card(i, j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
